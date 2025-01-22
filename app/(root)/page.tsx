862b91d39a8be70988930e2a11f2a9c44a685ed4
Egehan Gur <53318302+Eg3h4n@users.noreply.github.com>
2025-01-22 13:02:15 +0300

    card ui implementation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,24 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({searchParams}: { searchParams: Promise<{ query?: string }> }) {
     const query = (await searchParams).query;
+
+    const posts = [
+        {
+            _createdAt: new Date(),
+            views: 55,
+            author: {
+                _id: 1,
+                name: 'Egehan',
+            },
+            _id: 1,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id turpis quis erat tincidunt tincidunt.',
+            image: 'https://placehold.co/400x200',
+            category: 'Robotics',
+            title: 'Quad Robotics',
+        },
+    ]
 
     return (
         <>
@@ -13,6 +30,23 @@ export default async function Home({searchParams}: { searchParams: Promise<{ que
                 </p>
 
                 <SearchForm query={query}/>
+            </section>
+
+            <section className="section_container">
+                <p className="text-30-semibold">
+                    {query ? `Search results for "${query}"` : "All Startups"}
+                </p>
+
+                <ul className="mt-7 card_grid">
+                    {posts?.length > 0 ? (
+                        posts.map((post: StartupCardType, index: number) => (
+                            <StartupCard key={post?._id} post={post}/>
+                        ))
+                    ) : (
+                        <p className="no-results">No startups found</p>
+                    )}
+                </ul>
+
             </section>
         </>
     );
